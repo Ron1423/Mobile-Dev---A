de8de47a0fb7b128250de47e25161a8c53903b0f
Ron1423 <s2200605@student.unklab.ac.id>
2023-05-12 17:46:09 +0800

    New Commit

---
+++ DondonApp/src/screens/index.js
@@ -0,0 +1,10 @@
+import Splash from "./Splash";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import Home from "./Home";
+import Menus from "./Menus";
+import Intro from "./Intro";
+import OrderSummary from "./OrderSummary";
+import AboutUs from "./AboutUs";
+
+export { Splash,Intro,SignIn,SignUp,Home,Menus,OrderSummary,AboutUs };
